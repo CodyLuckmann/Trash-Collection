7faa2ee748d88bbb5e72bf91a4ffde485f719204
CodyLuckmann <cluckmann@live.com>
2021-12-14 14:13:32 -0600

    uncommented create and index functions. added edit function

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -18,35 +18,56 @@ def index(request):
     Customer = apps.get_model('customers.Customer')
     return render(request, 'employees/index.html')
 
-# @login_required
-# def index(request):
+@login_required
+def index(request):
     # The following line will get the logged-in user (if there is one) within any view function
-    # logged_in_user = request.user
-    # try:
+    logged_in_user = request.user
+    try:
         # This line will return the customer record of the logged-in user if one exists
-    #     logged_in_employee = Employee.objects.get(user=logged_in_user)
+        logged_in_employee = Employee.objects.get(user=logged_in_user)
 
-    #     today = date.today()
+        today = date.today()
         
-    #     context = {
-    #         'logged_in_employee': logged_in_employee,
-    #         'today': today
-    #     }
-    #     return render(request, 'employees/index.html', context)
-    # except ObjectDoesNotExist:
-    #     return HttpResponseRedirect(reverse('employees:create'))
+        context = {
+            'logged_in_employee': logged_in_employee,
+            'today': today
+        }
+        return render(request, 'employees/index.html', context)
+    except ObjectDoesNotExist:
+        return HttpResponseRedirect(reverse('employees:create'))
     
 
-# @login_required
-# def create(request):
-#     logged_in_user = request.user
-#     if request.method == "POST":
-#         name_from_form = request.POST.get('name')
-#         address_from_form = request.POST.get('address')
-#         zip_from_form = request.POST.get('zip_code')
-#         weekly_from_form = request.POST.get('weekly_pickup')
-#         new_employee = Employee(name=name_from_form, user=logged_in_user, address=address_from_form, zip_code=zip_from_form, weekly_pickup=weekly_from_form)
-#         new_employee.save()
-#         return HttpResponseRedirect(reverse('employee:index'))
-#     else:
-#         return render(request, 'employees/create.html')
+@login_required
+def create(request):
+    logged_in_user = request.user
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        address_from_form = request.POST.get('address')
+        zip_from_form = request.POST.get('zip_code')
+        weekly_from_form = request.POST.get('weekly_pickup')
+        new_employee = Employee(name=name_from_form, user=logged_in_user, address=address_from_form, zip_code=zip_from_form, weekly_pickup=weekly_from_form)
+        new_employee.save()
+        return HttpResponseRedirect(reverse('employee:index'))
+    else:
+        return render(request, 'employees/create_employee.html')
+    
+@login_required
+def edit_profile(request):
+    logged_in_user = request.user
+    logged_in_customer = Employee.objects.get(user=logged_in_user)
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        address_from_form = request.POST.get('address')
+        zip_from_form = request.POST.get('zip_code')
+        weekly_pickup_from_form = request.POST.get('weekly')
+        logged_in_customer.name = name_from_form
+        logged_in_customer.address = address_from_form
+        logged_in_customer.zip_code = zip_from_form
+        logged_in_customer.weekly_pickup = weekly_pickup_from_form
+        logged_in_customer.save()
+        return HttpResponseRedirect(reverse('employee:index'))
+    else:
+        context = {
+            'logged_in_customer': logged_in_customer
+        }
+        return render(request, 'employee/edit_employee_profile.html', context)
