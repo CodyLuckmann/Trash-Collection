802c6a99d7aec372057054591eb500c215a38902
CodyLuckmann <cluckmann@live.com>
2021-12-14 13:30:38 -0600

    added login for employee and create page

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,6 +1,12 @@
-from django.http import HttpResponse
-from django.shortcuts import render
 from django.apps import apps
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from datetime import date
+
+from .models import Employee
 
 # Create your views here.
 
@@ -11,3 +17,36 @@ def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
     Customer = apps.get_model('customers.Customer')
     return render(request, 'employees/index.html')
+
+# @login_required
+# def index(request):
+    # The following line will get the logged-in user (if there is one) within any view function
+    # logged_in_user = request.user
+    # try:
+        # This line will return the customer record of the logged-in user if one exists
+    #     logged_in_employee = Employee.objects.get(user=logged_in_user)
+
+    #     today = date.today()
+        
+    #     context = {
+    #         'logged_in_employee': logged_in_employee,
+    #         'today': today
+    #     }
+    #     return render(request, 'employees/index.html', context)
+    # except ObjectDoesNotExist:
+    #     return HttpResponseRedirect(reverse('employees:create'))
+    
+
+# @login_required
+# def create(request):
+#     logged_in_user = request.user
+#     if request.method == "POST":
+#         name_from_form = request.POST.get('name')
+#         address_from_form = request.POST.get('address')
+#         zip_from_form = request.POST.get('zip_code')
+#         weekly_from_form = request.POST.get('weekly_pickup')
+#         new_employee = Employee(name=name_from_form, user=logged_in_user, address=address_from_form, zip_code=zip_from_form, weekly_pickup=weekly_from_form)
+#         new_employee.save()
+#         return HttpResponseRedirect(reverse('employee:index'))
+#     else:
+#         return render(request, 'employees/create.html')
